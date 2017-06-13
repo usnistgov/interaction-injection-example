2e89ee2fa6a61f4680fcffef23576c47671ebf59
gcr-nist <geoffry.roberts@nist.gov>
2017-06-13 15:27:38 -0400

    Added Readme.md

--- src/main/java/gov/nist/hla/ii/ExampleInject.java
+++ src/main/java/gov/nist/hla/ii/ExampleInject.java
@@ -3,19 +3,16 @@ package gov.nist.hla.ii;
 import gov.nist.hla.ii.exception.PropertyNotAssigned;
 import gov.nist.hla.ii.exception.PropertyNotFound;
 import gov.nist.hla.ii.exception.RTIAmbassadorException;
-import hla.rti.ConcurrentAccessAttempted;
 import hla.rti.FederateNotExecutionMember;
 import hla.rti.NameNotFound;
-import hla.rti.ObjectClassNotDefined;
-import hla.rti.ObjectClassNotPublished;
 import hla.rti.ObjectNotKnown;
 import hla.rti.RTIinternalError;
-import hla.rti.RestoreInProgress;
-import hla.rti.SaveInProgress;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -29,30 +26,46 @@ public class ExampleInject implements Runnable {
 	InjectionFederate federate;
 	InterObjInjection inj;
 
+	Set<InterObjDef> intObjs = new HashSet<InterObjDef>();
+
 	public void init(String[] args) {
 		try {
+			// We instantiate the injection federate and configure it and
+			// initialize it in that order.
 			federate = new InjectionFederate();
 			federate.loadConfiguration(args[0]);
 			inj = new InterObjInjectionImpl(federate);
 			federate.setInterObjectInjection(inj);
 			federate.init();
+
+			// We define all interactions and objects we intend to inject. These
+			// must match what is in the FOM file.
+			// Names must be formatted correctly.
 			String interactionName = inj.formatInteractionName("Int1");
+			InterObjDef interactionDef = new InterObjDef(interactionName,
+					new HashMap<String, String>(), InterObjDef.TYPE.INTERACTION);
+			intObjs.add(interactionDef);
 			federate.publishInteraction(interactionName);
+
+			// Names must be formatted correctly.
 			String className = inj.formatObjectName("Obj1");
 			Map<String, String> attrMap = new HashMap<String, String>();
 			attrMap.put("Obj1Attr1", "XXX");
-			int classHandle = federate.publishObject(className, attrMap);
-			log.debug("classHandle=" + classHandle);
-			int objectHandle = federate.registerObject(className, classHandle);
-			log.debug("objectHandle=" + objectHandle);
+			InterObjDef objectDef = new InterObjDef(interactionName,
+					new HashMap<String, String>(), InterObjDef.TYPE.OBJECT);
+			intObjs.add(objectDef);
+
+			// We must set an object to receive interactions and object sent by
+			// other federates.
 			ExampleReception recp = new ExampleReception();
 			federate.setInterObjectReception(recp);
+
+			// The injector must be in its own thread.
 			Thread th = new Thread(federate);
 			th.start();
 		} catch (RTIAmbassadorException | ParserConfigurationException
 				| IOException | PropertyNotFound | PropertyNotAssigned e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.error(e);
 		}
 
 	}
@@ -61,22 +74,14 @@ public class ExampleInject implements Runnable {
 
 		double timeStep = 1D;
 		while (true) {
-			String interactionName = inj.formatInteractionName("Int1");
-			log.trace("injecting interactionName=" + interactionName);
-			Map<String, String> paramMap = new HashMap<String, String>();
-			inj.addInteraction(interactionName, paramMap);
-
-			String objectName = inj.formatObjectName("Obj1");
-			Map<String, String> attrMap = new HashMap<String, String>();
-			attrMap.put("Obj1Attr1", "XXX-" + timeStep);
-
-			try {
-				inj.addObject(objectName, attrMap);
-			} catch (NameNotFound | FederateNotExecutionMember
-					| RTIinternalError | ObjectNotKnown e) {
-				log.error(e);
+			// In this example we run through our collection of federates.
+			// Here, we are staging each one to be sent byt the injector in the current time step.
+			for (InterObjDef def : intObjs) {
+				inj.addInterObj(def);
 			}
+
 			try {
+				// We tell the injector the staging in done and to advance its time step
 				timeStep = federate.advanceLogicalTime();
 			} catch (RTIAmbassadorException e) {
 				log.error(e);
