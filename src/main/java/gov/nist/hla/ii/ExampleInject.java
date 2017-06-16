cac0c7d82ed027a3a804c3a71ffb4e69cb236026
gcr-nist <geoffry.roberts@nist.gov>
2017-06-16 12:24:50 -0400

    Added a class to contain main

--- src/main/java/gov/nist/hla/ii/ExampleInject.java
+++ src/main/java/gov/nist/hla/ii/ExampleInject.java
@@ -1,111 +1,74 @@
 package gov.nist.hla.ii;
 
-import gov.nist.hla.ii.exception.PropertyNotAssigned;
-import gov.nist.hla.ii.exception.PropertyNotFound;
-import gov.nist.hla.ii.exception.RTIAmbassadorException;
+import hla.rti.FederateNotExecutionMember;
+import hla.rti.NameNotFound;
+import hla.rti.ObjectNotKnown;
+import hla.rti.RTIinternalError;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.xml.parsers.ParserConfigurationException;
+import java.util.Queue;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleInject implements Runnable {
+public class ExampleInject extends InterObjInjectionImpl {
 
 	private static final Logger log = LogManager.getLogger(ExampleInject.class);
 
 	InjectionFederate federate;
-	InterObjInjection inj;
+	InterObjInjection intObjs = new InterObjInjectionImpl();
 
-	Set<InterObjDef> intObjs = new HashSet<InterObjDef>();
+	public ExampleInject(InjectionFederate federate) {
+		super();
+		this.federate = federate;
+	}
 
-	public void init(String configPath) {
+	// public void init() {
+	// try {
+	// // We define all interactions and objects we intend to inject. These
+	// // must match what is in the FOM file.
+	// // Names must be formatted correctly.
+	// String interactionName = federate.formatInteractionName("Int1");
+	// intObjs.addInteraction(interactionName, new HashMap<String, String>());
+	//
+	// // Names must be formatted correctly.
+	// String className = federate.formatObjectName("Obj1");
+	// Map<String, String> attrMap = new HashMap<String, String>();
+	// attrMap.put("Obj1Attr1", "XXX");
+	// intObjs.addObject(className, attrMap);
+	//
+	// } catch (NameNotFound | FederateNotExecutionMember | RTIinternalError |
+	// ObjectNotKnown e) {
+	// log.error(e);
+	// }
+	// }
+
+	// public InterObjInjection getIntObjs() {
+	// return intObjs;
+	// }
+
+	@Override
+	public Queue<InterObjDef> getInteractions() {
+		log.trace("getInteractions==>");
 		try {
-			// We instantiate the injection federate, configure it and
-			// initialize it in that order.
-			federate = new InjectionFederate();
-			federate.loadConfiguration(configPath);
-			inj = new InterObjInjectionImpl(federate);
-			federate.setInterObjectInjection(inj);
-
-			// We must set an object to receive interactions and object sent by
-			// other federates.
-			ExampleReception recp = new ExampleReception();
-			federate.setInterObjectReception(recp);
-			federate.init();
-
 			// We define all interactions and objects we intend to inject. These
 			// must match what is in the FOM file.
 			// Names must be formatted correctly.
 			String interactionName = federate.formatInteractionName("Int1");
-			InterObjDef interactionDef = new InterObjDef(interactionName,
-					new HashMap<String, String>(), InterObjDef.TYPE.INTERACTION);
-			intObjs.add(interactionDef);
-			log.debug("staged interaction=" + interactionDef);
+			intObjs.addInteraction(interactionName,
+					new HashMap<String, String>());
 
 			// Names must be formatted correctly.
 			String className = federate.formatObjectName("Obj1");
 			Map<String, String> attrMap = new HashMap<String, String>();
 			attrMap.put("Obj1Attr1", "XXX");
-			InterObjDef objectDef = new InterObjDef(className,
-					attrMap, InterObjDef.TYPE.OBJECT);
-			intObjs.add(objectDef);
-			log.debug("staged object=" + objectDef);
+			intObjs.addObject(className, attrMap);
 
-			// The injector must be in its own thread.
-			Thread th = new Thread(federate);
-			th.start();
-		} catch (RTIAmbassadorException | ParserConfigurationException
-				| IOException | PropertyNotFound | PropertyNotAssigned e) {
+		} catch (NameNotFound | FederateNotExecutionMember | RTIinternalError
+				| ObjectNotKnown e) {
 			log.error(e);
 		}
-	}
-
-	public void run() {
-
-		double timeStep = 0D;
-		log.info("timeStep=" + timeStep);
-		while (federate.getState() != InjectionFederate.State.TERMINATING) {
-			// In this example we run through our collection of federates.
-			// Here, we are staging each one to be sent by the injector in the
-			// current time step.
-			for (InterObjDef def : intObjs) {
-				inj.addInterObj(def);
-			}
-
-			// We tell the injector the staging in done and to advance its
-			// time step
-			timeStep = federate.startLogicalTime();
-			while (federate.getAdvancing().get()) {
-				try {
-					log.trace("sleeping==>");
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					log.error(e);
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			log.error("command line argument for properties file not specified");
-			args = new String[1];
-			args[0] = "config.properties";
-		}
-
-		try {
-			ExampleInject app = new ExampleInject();
-			app.init(args[0]);
-			app.run();
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-		}
+		return intObjs.getInteractions();
 	}
 }
